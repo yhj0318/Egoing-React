b490b33bf431f337f1ba8d5cb8906be9eeea5f9d
yhj0318 <gudwns0318@naver.com>
2023-08-24 14:59:30 +0900

    props update

--- src/App.js
+++ src/App.js
@@ -2,34 +2,44 @@ import logo from './logo.svg';
 import './App.css';
 function Header(props) // 함수로 만들때는 무조건 대문자로 시작해야함
 {
-  console.log('props', props);
+  console.log('props', props, props.title);
   return <header>
-  <h1><a href='/'>WEB</a></h1>
+  <h1><a href='/'>{props.title}</a></h1> {/* { }는 표현식으로 취급되기 때문에 해석되서 값이 웹에 반영된다. */}
   </header>
 }
-function Nav()
+function Nav(props)
 {
+  const lis = []
+  for(let i= 0 ;i<props.topics.length; i++)
+  {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+  }
   return <nav>
   <ol>
-    <li><a href='/read/1'>html</a></li>
-    <li><a href='/read/2'>css</a></li>
-    <li><a href='/read/3'>js</a></li>
+    {lis}
   </ol>
 </nav>
 }
-function Article()
+function Article(props)
 {
   return <article>
-  <h2>Welcome</h2>
-  Hello, WEB
+  <h2>{props.title}</h2>
+  {props.body}
 </article>
 }
 function App() {
+  const topics =
+  [
+    {id:1, title:"html", body:"htmp is ..."},
+    {id:2, title:"css", body:"css is ..."},
+    {id:3, title:"javascript", body:"javascript is ..."}
+  ]
   return (
-    <div>
-      <Header title="REACT"></Header>
-      <Nav></Nav>
-      <Article></Article>
+    <div> 
+      <Header title="WEB"></Header> {/* 리액트의 속성을 props라고 함 */}
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
 }
